b543cf37d5aa66cd513bc8a2ba9612aa3c574fd4
dseif <david.c.seifried@gmail.com>
2013-03-06 15:16:11 -0500

    Added videojs embed demo.

---
+++ examples/js/videojs-embed.js
@@ -0,0 +1,24 @@
+(function() {
+    var hapyakEditor = hapyak.editor({
+        elementId: 'hapyak-editor',
+        environment: 'staging',
+        width: 560,
+        videoId: 607159,
+        trackId: 2367,
+        onLoadTrack: function (p) {
+            console.log('hapyak.editor.onLoadTrack reported to parent page [' + p.videoId + ', ' + p.trackId + ']');
+        },
+        onLoadCurrentUser: function (p) {
+            console.log('hapyak.editor.onLoadCurrentUser reported to parent page [' + p.userId + ', ' + p.username + ']');
+        },
+        onNewTrack: function (trackId) {
+            console.log('hapyak.editor.onNewTrack reported to parent page [' + trackId + ']');
+        },
+        onSave: function (trackId) {
+            console.log('hapyak.editor.onSave reported to parent page [' + trackId + ']');
+        },
+        onLoad: function () {
+            console.log('hapyak.editor.onLoad reported to parent page');
+        }
+    });
+})();
