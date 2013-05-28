6d17387922ef9a04e06f4f7706edfebddf3a9684
dseif <david.c.seifried@gmail.com>
2013-05-28 18:30:16 -0400

    Added dailymotion embed demo.

---
+++ examples/js/dailymotion-embed.js
@@ -0,0 +1,23 @@
+(function() {
+    var hapyakEditor = hapyak.editor({
+        elementId: 'hapyak-editor',
+        environment: 'staging',
+        videoId: 526426,
+        trackId: 1627,
+        onLoadTrack: function (p) {
+            console.log('hapyak.editor.onLoadTrack reported to parent page [' + p.videoId + ', ' + p.trackId + ']');
+        },
+        onLoadCurrentUser: function (p) {
+            console.log('hapyak.editor.onLoadCurrentUser reported to parent page [' + p.userId + ', ' + p.username + ']');
+        },
+        onNewTrack: function (trackId) {
+            console.log('hapyak.editor.onNewTrack reported to parent page [' + trackId + ']');
+        },
+        onSave: function (trackId) {
+            console.log('hapyak.editor.onSave reported to parent page [' + trackId + ']');
+        },
+        onLoad: function () {
+            console.log('hapyak.editor.onLoad reported to parent page');
+        }
+    });
+})();
